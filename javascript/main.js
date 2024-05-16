039155e90dc7f2fad685da6c7efa9fe555e4375b
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-16 11:49:27 +0200

    [UPD] Minor fixes

--- javascript/main.js
+++ javascript/main.js
@@ -4,7 +4,7 @@
 const images = [
   {
     image: "img/01.webp",
-    title: "Marvel's Spiderman Miles Morale",
+    title: "Marvel's Spiderman Miles Morales",
     text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
   },
   {
